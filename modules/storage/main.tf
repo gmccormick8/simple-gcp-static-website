786202bccd10fb5e37b44edf2ec525e442014269
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-01 20:04:59 -0700

    checkov testing

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,12 +3,9 @@ resource "random_integer" "bucket_name_prefix" {
   max = 9999999
 }
 
-#checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
 module "gcs_buckets" {
   source        = "git::https://github.com/terraform-google-modules/terraform-google-cloud-storage.git?ref=66e472b56cd21b45a3939883a66ccfcf5ec1b9ed"
-  #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
   project_id    = var.project_id
-  #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
   names         = [var.bucket_name]
   prefix        = random_integer.bucket_name_prefix.result
   location      = "US"
@@ -21,12 +18,9 @@ module "gcs_buckets" {
 }
 
 # Make bucket public by granting allUsers storage.objectViewer access
-#checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
 resource "google_storage_bucket_iam_member" "public_rule" {
+  bucket = module.gcs_buckets.bucket.id
+  role   = "roles/storage.objectViewer"
   #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
-  bucket = module.gcs_buckets.bucket.id #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
-  #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
-  role   = "roles/storage.objectViewer" #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
-  #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
-  member = "allUsers" #checkov:skip=CKV_GCP_28:Skip public bucket access check, required for static website hosting
+  member = "allUsers"
 }
