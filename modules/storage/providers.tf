95cd03e3423221cdb51d4fc87081fc999bc96663
Gabriel McCormick <74574750+gmccormick8@users.noreply.github.com>
2025-05-14 10:34:47 -0700

    Update and rename versions.tf to providers.tf

--- modules/storage/providers.tf
+++ modules/storage/providers.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "~> 1.11.0"
+  required_version = "~> 1.11"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.30.0"
+      version = "~> 6.30"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.7.0"
+      version = "~> 3.7"
     }
   }
 }
